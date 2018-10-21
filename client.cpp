2a3a895ad2383bda8fc68b0b52606a2cf4c949f2
jonathan-innis <jonathan.innis.ji@gmail.com>
2018-10-21 16:55:23 -0500

    Updated the report

--- client.cpp
+++ client.cpp
@@ -57,20 +57,6 @@ void* request_thread_function(void* arg) {
 }
 
 void* worker_thread_function(void* arg) {
-    /*
-		Fill in this function. 
-
-		Make sure it terminates only when, and not before,
-		all the requests have been processed.
-
-		Each thread must have its own dedicated
-		RequestChannel. Make sure that if you
-		construct a RequestChannel (or any object)
-		using "new" that you "delete" it properly,
-		and that you send a "quit" request for every
-		RequestChannel you construct regardless of
-		whether you used "new" for it.
-     */
     worker_struct* args = (worker_struct*) arg;
 
     while(true) {
